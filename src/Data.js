dcfdf02db0c6fff2fbd1e56d30adb3271719a776
popoolahussein <popoolakehindehussein@gmail.com>
2024-09-10 20:24:20 +0100

    docs: adjusted img file path

--- src/Data.js
+++ src/Data.js
@@ -4,97 +4,97 @@ export const shopItemsData = [
         name: "Flower-vase",
         price: 3500,
         desc: "lorem ipsum dolor sit amet consectetur adipisicing.",
-        img: "src/img/flower-vase.jpg"
+        img: "pubic/img/flower-vase.jpg"
     },
     {
         id: "hjisneyakmzh",
         name: "Flat side bag",
         price: 5000,
         desc: "lorem ipsum dolor sit amet consectetur adipisicing.",
-        img: "src/img/side-bag2.jpg"
+        img: "pubic/img/side-bag2.jpg"
     },
     {
         id: "lkievgyyamc",
         name: "Ladies bag",
         price: 10000,
         desc: "lorem ipsum dolor sit amet consectetur adipisicing.",
-        img: "src/img/School-bag.jpg"
+        img: "pubic/img/School-bag.jpg"
     },
     {
         id: "ljgvczmqhaabc",
         name: "Waist bead",
         price: 1000,
         desc: "lorem ipsum dolor sit amet consectetur adipisicing.",
-        img: "src/img/waist bead2.jpg"
+        img: "pubic/img/waist bead2.jpg"
     },
     {
         id: "lodlewh",
         name: "Bead bag",
         price: 5000,
         desc: "lorem ipsum dolor sit amet consectetur adipisicing.",
-        img: "src/img/bag.jpg"
+        img: "pubic/img/bag.jpg"
     },
     {
         id: "kdglkuheu",
         name: "Ring",
         price: 2000,
         desc: "lorem ipsum dolor sit amet consectetur adipisicing.",
-        img: "src/img/ring.jpg"
+        img: "pubic/img/ring.jpg"
     },
     {
         id: "avzikjdn",
         name: "Couple-bracelet",
         price: 4000,
         desc: "lorem ipsum dolor sit amet consectetur adipisicing.",
-        img: "src/img/couple-bracelet.jpg"
+        img: "pubic/img/couple-bracelet.jpg"
     },
     {
         id: "kgdqle",
         name: "Choker",
         price: 3500,
         desc: "lorem ipsum dolor sit amet consectetur adipisicing.",
-        img: "src/img/choker.jpg"
+        img: "pubic/img/choker.jpg"
     },
     {
         id: "ojdgfaj",
         name: "Key-holder",
         price: 500,
         desc: "lorem ipsum dolor sit amet consectetur adipisicing.",
-        img: "src/img/key-holder.jpg"
+        img: "pubic/img/key-holder.jpg"
     },
     {
         id: "udfhbhhj",
         name: "Men-bracelet",
         price: 3000,
         desc: "lorem ipsum dolor sit amet consectetur adipisicing.",
-        img: "src/img/men-bracelet.jpg"
+        img: "pubic/img/men-bracelet.jpg"
     },
     {
         id: "jhnkkwe",
         name: "Side bag",
         price: 5000,
         desc: "lorem ipsum dolor sit amet consectetur adipisicing.",
-        img: "src/img/side-bag.jpg"
+        img: "pubic/img/side-bag.jpg"
     },
     {
         id: "ncvuqkq",
         name: "Bracelet-ring",
         price: 3500,
         desc: "lorem ipsum dolor sit amet consectetur adipisicing.",
-        img: "src/img/Bracelet-ring.jpg"
+        img: "pubic/img/Bracelet-ring.jpg"
     },
     {
         id: "poeefw",
         name: "Leg chain",
         price: 2500,
         desc: "lorem ipsum dolor sit amet consectetur adipisicing.",
-        img: "src/img/leg-chain.jpg"
+        img: "pubic/img/leg-chain.jpg"
     },
     {
         id: "vh0kQHQK",
         name: "Leg chain",
         price: 4000,
         desc: "lorem ipsum dolor sit amet consectetur adipisicing.",
-        img: "src/img/leg-chain2.jpg"
+        img: "pubic/img/leg-chain2.jpg"
     }
 ];
